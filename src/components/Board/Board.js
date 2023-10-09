6af2075e7dc269f14266bdbcb3faae65af122243
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-08 21:08:30 -0700

    imported context for using categories

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import JeopardyContext from '../../JeopardyContext'
 import CategoryHeader from './CategoryHeader'
 import Clue from './Clue'
 
 const Board = () => {
+  const {
+    categories
+  } = useContext( JeopardyContext )
+
+  console.log('kjsfkjasdkfj', categories)
+  
   return (
     <>
-      <CategoryHeader />
+      <CategoryHeader>Header1</CategoryHeader>
       <Clue>one clue</Clue>
     </>
   )
