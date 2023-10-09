e8c0ee6dca52cc54e3bd0299c4412249124531d7
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-08 21:06:21 -0700

    import useContext, new state added for handling initializing game, function start for starting game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { getCategoryIds } from './api/api';
 import Home from './components/home/Home';
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import JeopardyContext from './JeopardyContext'
 
 function App() {
-  const [categories, setCategories ] = useState([])
+  const [ categories, setCategories ] = useState( [] )
+  const [ isPlaying, setIsPlaying ] = useState( false )
+
+  const startGame = async () => {
+    const catIds = await getCategoryIds()
+    setCategories( prev => [ ...prev, ...catIds ] )
+    setIsPlaying( true )
+  }
 
   return (
-    <JeopardyContext.Provider value={{ categories, setCategories }}>
+    <JeopardyContext.Provider value={{ 
+      categories, 
+      setCategories,
+      getCategoryIds,
+      isPlaying,
+      setIsPlaying,
+      startGame
+      }}>
       <Home />
     </JeopardyContext.Provider>
   );
