914ae1bfd2152143a3c80533b69fa2450fea7bfd
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-08 19:55:48 -0700

    file board added, this will render the Jeopardy board game

---
+++ src/components/Board/Board.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CategoryHeader from './CategoryHeader'
+import Clue from './Clue'
+
+const Board = () => {
+  return (
+    <>
+      <CategoryHeader />
+      <Clue>one clue</Clue>
+    </>
+  )
+}
+
+export default Board
