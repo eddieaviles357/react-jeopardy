93a41f4cade37437cb6ed23157456c0763175b5e
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-11 21:04:01 -0700

    removed inline style and updated with classes

--- src/components/home/Title.js
+++ src/components/home/Title.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import logo from '../../Jeopardy-logo.svg';
-
-const titleStyle = {
-  display: 'block',
-  width: '500px',
-  margin: '0 auto',
-  padding: '10px'
-}
+import logo from '../../Jeopardy-logo.svg'
+import './home.css'
 
 const Title = () => {
   return (
     <h1>
-      <img src={logo} style={titleStyle} className="main-title" alt="Jeopardy" />
+      <img src={logo} className="main-title" alt="Jeopardy" />
     </h1>
   )
 }
