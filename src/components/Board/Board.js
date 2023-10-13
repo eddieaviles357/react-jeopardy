19a115902d857bcee36332dcb6dc4bfef8254391
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-12 21:27:24 -0700

    reformat

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,25 +2,30 @@ import React, { useContext } from 'react'
 import JeopardyContext from '../../JeopardyContext'
 import CategoryHeader from './CategoryHeader'
 import Clue from './Clue'
-import { NUM_CATEGORIES } from '../../constants'
+import { NUM_CLUES } from '../../constants'
 
 const Board = () => {
-  const {
-    jpardyData
-  } = useContext( JeopardyContext )
+  const { jpardyData } = useContext( JeopardyContext )
+  
+  // question, answer, showing
+  const jpardyClues = ( cluesArr, idx1 ) => (
+    cluesArr.map( (clue, idx2) => ( 
+      <Clue 
+        key={ new Date().getTime() + idx1 + idx2} 
+        id={ `${idx1} - ${idx2}` }
+        >{ clue.question }</Clue> 
+    ))
+  )
 
   console.log('Board.Component', jpardyData)
+  // const defaultClue = {quesiton: 'NA', answer: 'NA', showing: null}
 
   return (
     <>
-      {jpardyData.map( ({ title, clues }, idx1 ) => ( 
-        <div class='Board-column' key={ new Date().getTime() + idx1 }>
+    {jpardyData.map( ({ title, clues }, idx1 ) => ( 
+        <div className='Board-column' key={ new Date().getTime() + idx1 }>
           <CategoryHeader>{ title }</CategoryHeader> 
-            {clues.map( ({ question, answer, showing }, idx2 ) => ( 
-              <Clue key={ new Date().getTime() + idx1 + idx2} id={ `${idx1} - ${idx2}` } >
-              { question }
-              </Clue> 
-            ))}
+          {jpardyClues(clues, idx1)}
         </div>
       ))}
     </>
