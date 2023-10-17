5601b00e8672d516ab1c6f0060fe09304fdbb446
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-16 22:12:50 -0700

    default object added for clues

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,9 @@ export const NUM_CATEGORIES = 6;
 export const NUM_CLUES = 5;
 
 export const BASE_URL = 'http://jservice.io/api/';
+
+export const DEFAULT_CLUE = {
+  question: 'What do you call a depressed coffee', 
+  answer: 'depresso',
+  showing: null
+}
