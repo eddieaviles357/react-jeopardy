bb382a25222f739b00e6f3233be0e2750bf51813
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-16 22:11:59 -0700

    checks against an empty clues array and returns default values

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,23 +2,33 @@ import React, { useContext } from 'react'
 import JeopardyContext from '../../JeopardyContext'
 import CategoryHeader from './CategoryHeader'
 import Clue from './Clue'
-import { NUM_CLUES } from '../../constants'
+import { DEFAULT_CLUE, NUM_CLUES } from '../../constants'
 
 const Board = () => {
   const { jpardyData } = useContext( JeopardyContext )
   
   // question, answer, showing
-  const jpardyClues = ( cluesArr, idx1 ) => (
+  const jpardyClues = ( cluesArr, idx1 ) => {
+    // if api returns an empty array of clues use default values
+    if(cluesArr.length === 0) {
+      let numberOfClues = NUM_CLUES;
+
+      while(numberOfClues > 0) {
+        cluesArr.push( DEFAULT_CLUE )
+        numberOfClues--;
+      }
+    }
+
+    return (
     cluesArr.map( (clue, idx2) => ( 
       <Clue 
         key={ new Date().getTime() + idx1 + idx2} 
         id={ `${idx1} - ${idx2}` }
         >{ clue.question }</Clue> 
     ))
-  )
+  )}
 
   console.log('Board.Component', jpardyData)
-  // const defaultClue = {quesiton: 'NA', answer: 'NA', showing: null}
 
   return (
     <>
