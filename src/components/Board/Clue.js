f99393698158c10f1022d35ac89854391bb56c09
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-18 18:50:40 -0700

    handles click events displaying question then answer

--- src/components/Board/Clue.js
+++ src/components/Board/Clue.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './board.css'
 
-const Clue = ({ id, children }) => {
+const Clue = ({ id, question, answer, value, showing }) => {
+  const [isShowing, setIsShowing] = useState(showing)
+  
+  
+  const clickHandler = (e) => {
+    (isShowing === null) ? setIsShowing( true) : setIsShowing( false )
+    console.log(`CLUE::ID=>>>%c${id}`, "color: red")
+  }
+  
+  const content = 
+  (isShowing === null) ? value : (isShowing === true) ? question : answer
+
   return (
-    <div className='Board-clue' id={id}>{children}</div>
+    <div 
+      onClick={ clickHandler}
+      className='Board-clue' 
+      id={id}
+      >{ content }</div>
   )
 }
 
