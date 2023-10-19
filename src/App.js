17d793e970fbc344dd7ab761279ee57aaa6a625e
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-18 20:26:34 -0700

    new state added for avoiding multiple calls to api

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import JeopardyContext from './JeopardyContext'
 function App() {
   const [ jpardyData, setJpardyData ] = useState( [] )
   const [ isPlaying, setIsPlaying ] = useState( false )
+  // used to avoid multiple calls to api
+  const [ isClicked, setIsClicked ] = useState( false )
 
   const startGame = async () => {
+    if(isClicked) return;
+    setIsClicked( true );
     const boardData = await setBoardData()
 
     setJpardyData( boardData )
     
+    // wait 2 seconds so we can apply some animations
     setTimeout( () => setIsPlaying( true ), 2000 )
   }
 
