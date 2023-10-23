c4ebf1e5455e7712e1036239b4422f26f43be799
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-22 22:06:56 -0700

    animations added

--- src/components/home/PlayBtn.js
+++ src/components/home/PlayBtn.js
@@ -1,16 +1,22 @@
 import React, { useContext } from 'react'
 import './home.css'
 import JeopardyContext from '../../JeopardyContext'
+import { motion } from 'framer-motion'
 
 const PlayBtn = () => {
   const { startGame } = useContext( JeopardyContext )
   
   return (
-    <button 
+    <motion.button 
+      whileHover={{ scale: 1.2, boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.1)' }}
+      whileTap={{ scale: 1, backgroundColor: '#8585f0'}}
+      animate={{
+        boxShadow: "10px 10px 4px rgba(0, 0, 0, 0.2)",
+      }}
       onClick={startGame}
       className='Play-btn btn'>
       Play
-    </button>
+    </motion.button>
   )
 }
 
