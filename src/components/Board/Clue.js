1151a91b13e5c993295ae30b6269fb0b8db7e792
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-10-22 22:07:43 -0700

    click handler updated

--- src/components/Board/Clue.js
+++ src/components/Board/Clue.js
@@ -5,17 +5,20 @@ const Clue = ({ id, question, answer, value, showing }) => {
   const [isShowing, setIsShowing] = useState(showing)
   
   
-  const clickHandler = (e) => {
+  const clickHandler = (evt) => {
     (isShowing === null) ? setIsShowing( true) : setIsShowing( false )
-    console.log(`CLUE::ID=>>>%c${id}`, "color: red")
+    // const id = evt.target.id
+    // // extract index from clue
+    // const categoryIdx = +id[0]
+    // const clueIdx = +id[id.length - 1]
+    // console.log(categoryIdx, clueIdx)
   }
   
-  const content = 
-  (isShowing === null) ? value : (isShowing === true) ? question : answer
+  const content = (isShowing === null) ? value : (isShowing === true) ? question : answer
 
   return (
     <div 
-      onClick={ clickHandler}
+      onClick={ clickHandler }
       className='Board-clue' 
       id={id}
       >{ content }</div>
