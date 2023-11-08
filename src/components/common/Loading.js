56276f6f8b8091fbd7d129b337494efa938695f2
Eduardo Aviles <eduardoaviles357@yahoo.com>
2023-11-07 20:17:18 -0800

    loading component

---
+++ src/components/common/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Loading = () => {
+  return (
+    <motion.div className='Loading'>
+      Loading...
+    </motion.div>
+  )
+}
+
+export default Loading
