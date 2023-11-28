beb93da0f17b2d56abeb1d65cb118e31571e03d9
eduardo <eduardoaviles357@yahoo.com>
2023-11-27 21:49:19 -0800

    title tag added for description on svg

--- src/components/home/Title.js
+++ src/components/home/Title.js
@@ -22,7 +22,23 @@ const Title = () => {
   return (
     <motion.svg 
       ref={ref}
-      version="1.0" id="title" inkscapeversion="0.91 r13725" sodipodidocname="Jeopardy!.svg" xmlnscc="http://creativecommons.org/ns#" xmlnsdc="http://purl.org/dc/elements/1.1/" xmlnsinkscape="http://www.inkscape.org/namespaces/inkscape" xmlnsrdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlnssodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlnssvg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 282.6" enableBackground="new 0 0 1000 282.6" xmlSpace="preserve">
+      version="1.0" 
+      id="title" 
+      inkscapeversion="0.91 r13725" 
+      sodipodidocname="Jeopardy!.svg" 
+      xmlnscc="http://creativecommons.org/ns#" 
+      xmlnsdc="http://purl.org/dc/elements/1.1/" 
+      xmlnsinkscape="http://www.inkscape.org/namespaces/inkscape" 
+      xmlnsrdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" 
+      xmlnssodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" 
+      xmlnssvg="http://www.w3.org/2000/svg" 
+      xmlns="http://www.w3.org/2000/svg" 
+      xmlnsXlink="http://www.w3.org/1999/xlink" 
+      x="0px" y="0px" 
+      viewBox="0 0 1000 282.6" 
+      enableBackground="new 0 0 1000 282.6" 
+      xmlSpace="preserve">
+        <title id="jeopardy-title">Jeopardy</title>
         <path className='title-svg' id="rect4359" inkscapeconnector-curvature="0" d="M68.5,0v242.6c0,4.1-3.4,7.4-7.6,7.4h-17c-4.2,0-7.6-3.3-7.6-7.4v-75H0
           v94.6c0,11.3,9.1,20.4,20.5,20.4h65c11.3,0,20.5-9.1,20.5-20.4V0C106,0,68.5,0,68.5,0z M160.2,0c-20.7,0-37.3,9.7-37.3,21.7V259
           c0,12,9.4,23.6,30.1,23.6h70.3V250H170c-5.4,0-9.7-4.3-9.7-9.7v-30.2c0-5.4,4.3-9.7,9.7-9.7h53.3v-32.2h-52.6
