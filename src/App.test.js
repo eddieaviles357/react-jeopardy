3c7a41c8ce08984d8e041fa3aa778384373f4dd3
eduardo <eduardoaviles357@yahoo.com>
2023-11-29 21:12:04 -0800

    snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,8 @@ import App from './App';
 test('does not crash', () => {
   render(<App />);
 });
+
+test('matches snapshot', () => {
+  const app = render(<App />);
+  expect(app.asFragment()).toMatchSnapshot();
+});
