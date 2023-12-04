6767a8a60322faf24413162076a35b0312c4379d
eduardo <eduardoaviles357@yahoo.com>
2023-12-03 18:42:16 -0800

    test updated

--- src/components/home/Home.test.js
+++ src/components/home/Home.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Home from './Home';
 import { useState } from 'react'
 import JeopardyContext from '../../JeopardyContext'
@@ -28,3 +28,26 @@ test('matches snapshot', () => {
         </MockCtx>);
     expect(home.asFragment()).toMatchSnapshot();
 });
+
+describe('Component rendered test', () => {
+    it('should render button', () => {
+        render(
+            <MockCtx>
+                <Home />
+            </MockCtx>);
+        const btn = screen.getByText(/Play/i);
+        expect(btn.textContent).toBe('Play');
+        expect(btn.className).toBe('Play-btn btn')
+    });
+
+    it('should render Title', () => {
+        render(
+            <MockCtx>
+                <Home />
+            </MockCtx>);
+        const title = screen.getByText(/Jeopardy/i);
+        expect(title.textContent).toBe('Jeopardy');
+        expect(title.id).toBe('jeopardy-title');
+    })
+
+})
