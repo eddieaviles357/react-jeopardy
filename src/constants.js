f3dc0691c3978e7d0ff3859f56962d773460f877
eduardo <eduardoaviles357@yahoo.com>
2023-12-07 19:43:30 -0800

    MOTION_BTN_ATTR object used for motion gestures

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,26 @@ export const NUM_CLUES = 5;
 export const BASE_URL = 'http://jservice.io/api/';
 
 export const DEFAULT_CLUE = {
-  question: 'What do you call a depressed coffee', 
+  question: 'What do you call a depressed coffee',
   answer: 'depresso',
   value: 100,
   showing: null
 }
+
+export const MOTION_BTN_ATTR = {
+  hover: {
+    scale: 1.2,
+    boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.1)',
+    backgroundColor: 'rgb(0, 0, 198)'
+  },
+  tap: {
+    scale: 1,
+    backgroundColor: 'yellow',
+    border: '3px solid blue',
+    color: 'blue',
+  },
+  animate: {
+    boxShadow: "10px 10px 4px rgba(0, 0, 0, 0.2)",
+  }
+}
+
