daabc703bbb9f72da22e37c7e152a572f58bba6a
eduardo <eduardoaviles357@yahoo.com>
2023-12-07 19:42:46 -0800

    motion attributes for div extracted to constants folder

--- src/components/home/PlayBtn.js
+++ src/components/home/PlayBtn.js
@@ -1,42 +1,27 @@
 import React, { useContext } from 'react'
 import './home.css'
+import { MOTION_BTN_ATTR } from '../../constants'
 import JeopardyContext from '../../JeopardyContext'
 import Loading from '../common/Loading'
 import { motion } from 'framer-motion'
 
 const PlayBtn = () => {
-  const { startGame, isLoading } = useContext( JeopardyContext )
+  const { startGame, isLoading } = useContext(JeopardyContext)
 
-  const motionAttr = {
-    hover: { 
-      scale: 1.2, 
-      boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.1)',
-      backgroundColor: 'rgb(0, 0, 198)'
-      },
-    tap: { 
-      scale: 1, 
-      backgroundColor: 'yellow',
-      border: '3px solid blue',
-      color: 'blue',
-      },
-    animate: {
-      boxShadow: "10px 10px 4px rgba(0, 0, 0, 0.2)",
-    }
-  }
-  
+
   return (
     <>
-    { isLoading ?
+      {isLoading ?
         <Loading /> :
         <motion.button layout
-          initial={motionAttr.initial}
-          whileHover={motionAttr.hover}
-          whileTap={motionAttr.tap}
-          animate={motionAttr.animate}
+          initial={MOTION_BTN_ATTR.initial}
+          whileHover={MOTION_BTN_ATTR.hover}
+          whileTap={MOTION_BTN_ATTR.tap}
+          animate={MOTION_BTN_ATTR.animate}
           onClick={startGame}
           className='Play-btn btn'>
           Play
-        </motion.button> }
+        </motion.button>}
 
     </>
   )
