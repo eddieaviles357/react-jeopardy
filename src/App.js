4b7cff42d7904d7583a41d4db8e253b607182bb8
eduardo <eduardoaviles357@yahoo.com>
2023-12-11 20:24:06 -0800

    export updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import { setBoardData } from './api/api';
+import setBoardData from './api/api';
 import Home from './components/home/Home';
 import { useState } from 'react'
 import JeopardyContext from './JeopardyContext'
 
 function App() {
-  const [ jpardyData, setJpardyData ] = useState( [] )
-  const [ isPlaying, setIsPlaying ] = useState( false )
-  const [ isLoading, setIsLoading ] = useState( false )
+  const [jpardyData, setJpardyData] = useState([])
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   // used to avoid multiple calls to api
-  const [ isPlayedBtnStarted, setisPlayedBtnStarted ] = useState( false )
+  const [isPlayedBtnStarted, setisPlayedBtnStarted] = useState(false)
 
   const startGame = async () => {
     try {
-      if(isPlayedBtnStarted) return
-      setisPlayedBtnStarted( true )
-      setIsLoading( true )
-      setJpardyData( await setBoardData() )
-      setIsPlaying( true )
-      setIsLoading( false )
+      if (isPlayedBtnStarted) return
+      setisPlayedBtnStarted(true)
+      setIsLoading(true)
+      setJpardyData(await setBoardData())
+      setIsPlaying(true)
+      setIsLoading(false)
     } catch (err) {
       return <div>Sorry something went wrong.Please Refresh</div>
     }
   }
 
   return (
-    <JeopardyContext.Provider value={{ 
-      jpardyData, 
+    <JeopardyContext.Provider value={{
+      jpardyData,
       setJpardyData,
       isPlaying,
       setIsPlaying,
       startGame,
       isLoading,
-      }}>
+    }}>
       <Home />
     </JeopardyContext.Provider>
   );
