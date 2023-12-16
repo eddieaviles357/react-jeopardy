0f0be7a56d593a13561345a3688f822cb0550025
eduardo <eduardoaviles357@yahoo.com>
2023-12-15 20:56:31 -0800

    test for first and last child - do they exist?

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,8 +1,9 @@
 "use strict";
 
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Board from './Board'
 import MockContextProvider from '../../helpers/MockContextProvider';
+import { MOCK_TEST_DATA } from '../../constants'
 
 test('Does not crash', () => {
     render(
@@ -18,4 +19,17 @@ test('Matches snapshot', () => {
         </MockContextProvider>
     );
     expect(board.asFragment()).toMatchSnapshot();
+});
+
+describe('Makes sure first child and last child are on screen', () => {
+    const board = render(
+        <MockContextProvider>
+            <Board />
+        </MockContextProvider>
+    );
+    const firstClueElement = screen.getByText(MOCK_TEST_DATA[0].title);
+    expect(firstClueElement.textContent).toBe(MOCK_TEST_DATA[0].title);
+
+    const lastClueElement = screen.getByText(MOCK_TEST_DATA[MOCK_TEST_DATA.length - 1].title);
+    expect(lastClueElement.textContent).toBe(MOCK_TEST_DATA[MOCK_TEST_DATA.length - 1].title);
 })
