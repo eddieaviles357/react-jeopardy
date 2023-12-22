ce116b0a0d24d1c16f8276b49bc7fb306ce19c42
eduardo <eduardoaviles357@yahoo.com>
2023-12-21 22:20:55 -0800

    updated test

--- src/components/Board/ActiveClue.test.js
+++ src/components/Board/ActiveClue.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import ActiveClue from './ActiveClue'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 test('does not crash', () => {
     render(<ActiveClue />);
@@ -10,4 +10,39 @@ test('does not crash', () => {
 test('Matches snapshot', () => {
     const activeClue = render(<ActiveClue />);
     expect(activeClue.asFragment()).toMatchSnapshot();
+});
+
+describe('Displays component', () => {
+    const jData = {
+        question: 'One of the Teletubbies',
+        answer: 'Dipsy',
+    };
+    test('Go Back gets displayed', () => {
+        const activeClue = render(
+            <ActiveClue
+                question={jData.question}
+                answer={jData.answer}
+                isAnswered={false}
+                setIsAnswered={() => true}
+                setIsShowing={() => true}
+            />);
+
+        const goBack = screen.getByText(/go back/i);
+        expect(goBack.textContent).toBe('Go Back');
+    });
+
+    test('Go Back gets displayed', () => {
+        const activeClue = render(
+            <ActiveClue
+                question={jData.question}
+                answer={jData.answer}
+                isAnswered={false}
+                setIsAnswered={() => true}
+                setIsShowing={() => true}
+            />);
+
+        const revealAnswer = screen.getByText(/reveal answer/i);
+        expect(revealAnswer.textContent).toBe('Reveal Answer');
+    })
+
 });
